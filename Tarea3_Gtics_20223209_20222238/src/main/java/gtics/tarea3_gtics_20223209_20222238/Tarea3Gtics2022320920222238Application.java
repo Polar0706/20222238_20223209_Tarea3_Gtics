38a0c3ff271a2a587629df5db6d4824ee867d21e
Tony F. <a20223209@pucp.edu.pe>
2025-04-09 20:28:27 -0500

    Projecto Creado

---
+++ Tarea3_Gtics_20223209_20222238/src/main/java/gtics/tarea3_gtics_20223209_20222238/Tarea3Gtics2022320920222238Application.java
@@ -0,0 +1,13 @@
+package gtics.tarea3_gtics_20223209_20222238;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea3Gtics2022320920222238Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tarea3Gtics2022320920222238Application.class, args);
+    }
+
+}
---
+++ Tarea3_Gtics_20223209_20222238/src/test/java/gtics/tarea3_gtics_20223209_20222238/Tarea3Gtics2022320920222238ApplicationTests.java
@@ -0,0 +1,13 @@
+package gtics.tarea3_gtics_20223209_20222238;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea3Gtics2022320920222238ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
